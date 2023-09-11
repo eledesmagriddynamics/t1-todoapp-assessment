f637d04613ac71a08f7a6a850b4fbdbcca73b000
eledesmagriddynamics <104097569+eledesmagriddynamics@users.noreply.github.com>
2023-09-11 16:46:35 -0500

    UI setup (#1)
    Add function to app controller.
    Add bootstrap to index.html
    Create styles.css
    Add cors middleware to server.
    Install cors dependency

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
